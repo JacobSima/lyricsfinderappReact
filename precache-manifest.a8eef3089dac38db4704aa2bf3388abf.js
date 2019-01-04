0be218dd2c8c74f124c3abd9e8d85f9d7df651d8
Jacob Sima <simajacob2011@gmail.com>
2019-01-04 22:45:54 +0200

    Updates

---
+++ precache-manifest.a8eef3089dac38db4704aa2bf3388abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "078aa3f09332c2484dbfd7239ef39e2d",
+    "url": "/lyricsfinderappReact/static/media/spinner.078aa3f0.gif"
+  },
+  {
+    "revision": "8c7c8b8699af85d4a21b",
+    "url": "/lyricsfinderappReact/static/js/runtime~main.8c7c8b86.js"
+  },
+  {
+    "revision": "09074ddba493a2ab111d",
+    "url": "/lyricsfinderappReact/static/js/main.09074ddb.chunk.js"
+  },
+  {
+    "revision": "761cd71225a82557d379",
+    "url": "/lyricsfinderappReact/static/js/1.761cd712.chunk.js"
+  },
+  {
+    "revision": "09074ddba493a2ab111d",
+    "url": "/lyricsfinderappReact/static/css/main.bff4bc7f.chunk.css"
+  },
+  {
+    "revision": "cda4be9b35c3b94579df537005d40d99",
+    "url": "/lyricsfinderappReact/index.html"
+  }
+];
